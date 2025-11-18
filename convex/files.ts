8c7a89cfe6391c36b9187b584591ca55c0b79040
meongj <meongjiee@gmail.com>
2025-11-18 18:54:32 +0900

    ✨ feat: 요거트볼 목록 조회에 페이지네이션 기능 추가

--- convex/files.ts
+++ convex/files.ts
@@ -1,20 +1,27 @@
 // 업로드된 모든 요거트볼 목록 가져오기
 
+import {paginationOptsValidator} from "convex/server";
 import {query} from "./_generated/server";
 
+// 이미지 리스트 조회 (페이지네이션)
 export const listFiles = query({
-  handler: async (ctx) => {
-    // yogurtBowls 테이블에서 모든 레코드 가져오기
-    const bowls = await ctx.db.query("yogurtBowls").collect();
+  args: {
+    paginationOpts: paginationOptsValidator,
+  },
+  handler: async (ctx, args) => {
+    const paginationResult = await ctx.db.query("yogurtBowls").order("desc").paginate(args.paginationOpts);
 
     // 각 bowl의 이미지 URL 가져오기
     const bowlsWithUrls = await Promise.all(
-      bowls.map(async (bowl) => ({
+      paginationResult.page.map(async (bowl) => ({
         ...bowl,
         url: await ctx.storage.getUrl(bowl.imageStorageId),
       }))
     );
 
-    return bowlsWithUrls;
+    return {
+      ...paginationResult,
+      page: bowlsWithUrls,
+    };
   },
 });
