2b3e9cc4a52c6a16e4574d567c091b3c83263698
meongj <meongjiee@gmail.com>
2025-11-18 19:00:35 +0900

    ✨ feat: IntersectionObserver를 활용한 무한 스크롤 구현
      - usePaginatedQuery로 페이지네이션 기능 추가
      - IntersectionObserver를 사용한 자동 데이터 로딩
      - 하단 300px 전 미리 로딩으로 UX 개선

--- src/pages/BowlCardListPage.tsx
+++ src/pages/BowlCardListPage.tsx
@@ -1,33 +1,52 @@
-import {useQuery} from "convex/react";
+import {usePaginatedQuery} from "convex/react";
 import BowlCard from "../components/BowlCard";
 import {api} from "../../convex/_generated/api";
+import {useEffect, useRef} from "react";
 
 function BowlCardListPage() {
-  const files = useQuery(api.files.listFiles);
+  const observerRef = useRef<HTMLDivElement>(null);
+  const {results, status, loadMore} = usePaginatedQuery(api.files.listFiles, {}, {initialNumItems: 2});
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && status === "CanLoadMore") {
+          // 데이터 로드
+          loadMore(2);
+        }
+      },
+      {
+        threshold: 0.1, // 10% 보이면
+        rootMargin: "0px 0px 300px 0px", //하단영역  300px 전에 미리 로드
+      }
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    // cleanup
+    return () => observer.disconnect();
+  });
+
+  if (results.length === 0) {
+    return <div className="col-span-2 text-center py-10 text-gray-500">저장된 요거트볼이 없습니다</div>;
+  }
 
   return (
-    <div className="h-screen overflow-y-auto">
+    <div className="h-screen overflow-y-auto max-w-md mx-auto bg-amber-50">
       <div className="flex justify-center items-center py-8">
         <h2 className="text-xl font-medium">My YogurtBowl</h2>
       </div>
-      <div className="grid grid-cols-2 gap-7 p-20">
-        {files === undefined ? (
-          <div className="col-span-2 text-center py-10">로딩 중...</div>
-        ) : files.length === 0 ? (
-          <div className="col-span-2 text-center py-10 text-gray-500">저장된 요거트볼이 없습니다</div>
-        ) : (
-          files?.map((bowl) => {
-            return (
-              <BowlCard
-                key={bowl._id}
-                id={bowl._id}
-                image={bowl.url}
-                date={new Date(bowl.createdAt).toLocaleString()}
-              />
-            );
-          })
-        )}
+      <div className="grid grid-cols-2 gap-7 p-20 pt-10">
+        {results?.map((bowl) => {
+          return (
+            <BowlCard key={bowl._id} id={bowl._id} image={bowl.url} date={new Date(bowl.createdAt).toLocaleString()} />
+          );
+        })}
+        {status === "LoadingMore" && <div className="col-span-2 text-center py-10">로딩 중...</div>}
       </div>
+      <div ref={observerRef} className="h-1 " />
     </div>
   );
 }
