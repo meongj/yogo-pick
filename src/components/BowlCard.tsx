497367d53669981a8e5b6fc2c811e83b1a80f1d6
meongj <meongjiee@gmail.com>
2025-11-20 14:34:06 +0900

    ✨ feat: 요거트볼 카드 클릭 시 상세 페이지 이동 기능 추가

--- src/components/BowlCard.tsx
+++ src/components/BowlCard.tsx
@@ -2,12 +2,18 @@ interface BowlCardProps {
   id: string;
   image: string | null;
   date: string;
+  onClick: () => void;
 }
 
-function BowlCard({id, image, date}: BowlCardProps) {
+function BowlCard({ id, image, date, onClick }: BowlCardProps) {
   return (
-    <div className="aspect-square w-full overflow-hidden border-2 border-gray-200 rounded-lg shadow-md">
-      <img src={image || ""} alt="요거트볼 이미지" className="w-full h-full object-cover" />
+    <div
+      className="aspect-square w-full cursor-pointer overflow-hidden rounded-lg border-2 border-gray-200 shadow-md transition-all duration-300 hover:scale-110 hover:shadow-lg"
+      onClick={() => {
+        onClick(id);
+      }}
+    >
+      <img src={image || ""} alt="요거트볼 이미지" className="h-full w-full object-cover" />
     </div>
   );
 }
--- src/pages/BowlCardListPage.tsx
+++ src/pages/BowlCardListPage.tsx
@@ -1,13 +1,16 @@
-import {usePaginatedQuery} from "convex/react";
+import { usePaginatedQuery } from "convex/react";
 import BowlCard from "../components/BowlCard";
-import {api} from "../../convex/_generated/api";
-import {useEffect, useRef} from "react";
+import { api } from "../../convex/_generated/api";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const PAGE_SIZE = 3;
 
 function BowlCardListPage() {
   const observerRef = useRef<HTMLDivElement>(null);
-  const {results, status, loadMore} = usePaginatedQuery(api.files.listFiles, {}, {initialNumItems: PAGE_SIZE});
+  const { results, status, loadMore } = usePaginatedQuery(api.files.listFiles, {}, { initialNumItems: PAGE_SIZE });
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -33,24 +36,22 @@ function BowlCardListPage() {
 
   // 첫 페이지 로딩시
   if (status === "LoadingFirstPage") {
-    return <div className="col-span-2 text-center py-10 text-gray-500">로딩중..</div>;
+    return <div className="col-span-2 py-10 text-center text-gray-500">로딩중..</div>;
   }
   if (results.length === 0) {
-    return <div className="col-span-2 text-center py-10 text-gray-500">저장된 요거트볼이 없습니다</div>;
+    return <div className="col-span-2 py-10 text-center text-gray-500">저장된 요거트볼이 없습니다</div>;
   }
 
   return (
-    <div className="h-screen overflow-y-auto max-w-md mx-auto bg-amber-50">
-      <div className="flex justify-center items-center py-8">
+    <div className="mx-auto h-screen max-w-md overflow-y-auto bg-amber-50">
+      <div className="flex items-center justify-center py-8">
         <h2 className="text-xl font-medium">My YogurtBowl</h2>
       </div>
       <div className="grid grid-cols-2 gap-7 p-20 pt-10">
         {results?.map((bowl) => {
-          return (
-            <BowlCard key={bowl._id} id={bowl._id} image={bowl.url} date={new Date(bowl.createdAt).toLocaleString()} />
-          );
+          return <BowlCard key={bowl._id} id={bowl._id} image={bowl.url} date={new Date(bowl.createdAt).toLocaleString()} onClick={() => navigate(`/detail/${bowl._id}`)} />;
         })}
-        {status === "LoadingMore" && <div className="col-span-2 text-center py-10">로딩 중...</div>}
+        {status === "LoadingMore" && <div className="col-span-2 py-10 text-center">로딩 중...</div>}
       </div>
       <div ref={observerRef} aria-hidden="true" />
     </div>
