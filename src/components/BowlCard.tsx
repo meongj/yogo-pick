e835f558ce5d89a285bd67c34bb4c75d23016cfd
meongj <meongjiee@gmail.com>
2025-11-21 17:27:35 +0900

    ♻️ refactor: BowlCard와 Navbar 컴포넌트   개선

--- src/components/BowlCard.tsx
+++ src/components/BowlCard.tsx
@@ -13,11 +13,17 @@ function BowlCard({ image, onClick }: BowlCardProps) {
         onClick();
       }}
     >
-      <img
-        src={image || ""}
-        alt="요거트볼 이미지"
-        className="h-full w-full object-cover"
-      />
+      {image ? (
+        <img
+          src={image}
+          alt="생성된 요거트볼"
+          className="h-full w-full object-cover"
+        />
+      ) : (
+        <div className="flex h-full w-full items-center justify-center bg-gray-200">
+          <span className="text-gray-400">이미지 없음</span>
+        </div>
+      )}
     </div>
   );
 }
